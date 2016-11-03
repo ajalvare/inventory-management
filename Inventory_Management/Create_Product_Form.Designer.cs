2c2b9080f334d423ef66144cce02f9fde617d596
Annie Alvarez <ajalvare@uark.edu>
2016-11-03 13:29:13 -0500

    Add Create Product Screen

---
+++ Inventory_Management/Create_Product_Form.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Inventory_Management
+{
+    partial class Create_Product_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.NameTextbox = new System.Windows.Forms.TextBox();
+            this.Namelabel = new System.Windows.Forms.Label();
+            this.Desclabel = new System.Windows.Forms.Label();
+            this.DescriptionTextBox = new System.Windows.Forms.TextBox();
+            this.Pricelabel = new System.Windows.Forms.Label();
+            this.PriceTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // NameTextbox
+            // 
+            this.NameTextbox.Location = new System.Drawing.Point(12, 12);
+            this.NameTextbox.Name = "NameTextbox";
+            this.NameTextbox.Size = new System.Drawing.Size(100, 20);
+            this.NameTextbox.TabIndex = 1;
+            // 
+            // Namelabel
+            // 
+            this.Namelabel.AutoSize = true;
+            this.Namelabel.Location = new System.Drawing.Point(138, 15);
+            this.Namelabel.Name = "Namelabel";
+            this.Namelabel.Size = new System.Drawing.Size(35, 13);
+            this.Namelabel.TabIndex = 2;
+            this.Namelabel.Text = "Name";
+            // 
+            // Desclabel
+            // 
+            this.Desclabel.AutoSize = true;
+            this.Desclabel.Location = new System.Drawing.Point(138, 38);
+            this.Desclabel.Name = "Desclabel";
+            this.Desclabel.Size = new System.Drawing.Size(60, 13);
+            this.Desclabel.TabIndex = 4;
+            this.Desclabel.Text = "Description";
+            // 
+            // DescriptionTextBox
+            // 
+            this.DescriptionTextBox.Location = new System.Drawing.Point(12, 38);
+            this.DescriptionTextBox.Name = "DescriptionTextBox";
+            this.DescriptionTextBox.Size = new System.Drawing.Size(100, 20);
+            this.DescriptionTextBox.TabIndex = 3;
+            // 
+            // Pricelabel
+            // 
+            this.Pricelabel.AutoSize = true;
+            this.Pricelabel.Location = new System.Drawing.Point(138, 67);
+            this.Pricelabel.Name = "Pricelabel";
+            this.Pricelabel.Size = new System.Drawing.Size(31, 13);
+            this.Pricelabel.TabIndex = 6;
+            this.Pricelabel.Text = "Price";
+            // 
+            // PriceTextBox
+            // 
+            this.PriceTextBox.Location = new System.Drawing.Point(12, 64);
+            this.PriceTextBox.Name = "PriceTextBox";
+            this.PriceTextBox.Size = new System.Drawing.Size(100, 20);
+            this.PriceTextBox.TabIndex = 5;
+            // 
+            // Create_Product_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Pricelabel);
+            this.Controls.Add(this.PriceTextBox);
+            this.Controls.Add(this.Desclabel);
+            this.Controls.Add(this.DescriptionTextBox);
+            this.Controls.Add(this.Namelabel);
+            this.Controls.Add(this.NameTextbox);
+            this.Controls.Add(this.button1);
+            this.Name = "Create_Product_Form";
+            this.Text = "Create_Product_Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox NameTextbox;
+        private System.Windows.Forms.Label Namelabel;
+        private System.Windows.Forms.Label Desclabel;
+        private System.Windows.Forms.TextBox DescriptionTextBox;
+        private System.Windows.Forms.Label Pricelabel;
+        private System.Windows.Forms.TextBox PriceTextBox;
+    }
+}
